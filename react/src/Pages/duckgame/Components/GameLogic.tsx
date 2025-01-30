dbdefc904f5155050a3615414ea7bca0ff6a7445
Elbio Irala <elbioirala@gmail.com>
2025-01-30 15:34:36 -0300

    Refactor: Ahora el componente GameLogic usa los demas componentes, se agregan cambios en la informacion dada de ambas pagias

--- react/src/Pages/duckgame/Components/GameLogic.tsx
+++ react/src/Pages/duckgame/Components/GameLogic.tsx
@@ -1,103 +1,99 @@
+import { Box, Stack, styled, Typography } from "@mui/material";
 import React from "react";
-import { Accordion, AccordionSummary, AccordionDetails, Typography, Box } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import TitleAndDescription from "./TitleAndDescription";
+import AccordionListWithHeader from "./AccordionList";
+import ChallengesAndSolutions from "./ChallengesAndSolutions";
+
+
+const Container = styled(Box)(({ theme }) => ({
+    margin: 1,
+    background: "transparent",
+}));
+
+
+const GameDescription = `
+    GameMap gestiona todos los elementos 
+    fundamentales de la partida, desde plataformas y límites del mapa hasta la generación de objetos y el 
+    manejo de proyectiles. La gestión de colisiones se realiza mediante un sistema dual: CollisionManager para 
+    la detección de colisiones usando el modelo AABB, y CollisionEventManager para resolver estas 
+    interacciones entre los diferentes elementos del juego.
+    El núcleo de todo el juego se ejecuta a través del GameLoop, que coordina todas las acciones de los 
+    jugadores y actualiza el estado del juego en un orden específico, desde la ejecución de acciones 
+    individuales hasta la actualización del estado global del mapa, finalizando con la generación y 
+    distribución de mensajes de estado a todos los jugadores.
+`;
+
+
+const challengesAndSolutions = [
+    {
+        title: 'Gestión de colisiones',
+        challenge: 'Detectar y resolver distintos tipos de colisiones entre objetos como jugadores, balas, plataformas y spawners (Generadores de armas), proporcionando respuestas según el tipo de interacción.',
+        solution: 'Se usa la clase CollisionManager para solamente detectar colisiones y su tipo mediante el modelo AABB. La clase CollisionEventManager se usa para resolver dichas colisiones, actuando e informando a otras clases segun el tipo de colision.'
+    },
+    {
+        title: 'Interacción con spawners de armas',
+        challenge: 'Proveer armas de forma dinámica y aleatoria a los jugadores en el mapa.',
+        solution: 'Se integro la clase ItemSpawner, la cual los Players puede interactuar para obtener armas, esta clase genera armas aleatorias gracias a su interacion con una WeaponFactory (Fabrica de armas) para poder entregarselas a los jugadores.'
+    },
+    {
+        title: 'Control del flujo del juego',
+        challenge: 'Coordinar las acciones de los jugadores y su interacción con el mapa, asegurando una actualización sincronizada del estado del juego.',
+        solution: 'Se desarrolló la clase GameLoop, que organiza el ciclo del juego de manera ordenada para evitar inconsistencias en las ejecuciones de las interacciones de los jugadores y los objetos garantizando siempre un orden especifico donde cada cosa sucede en el juego.'
+    },
+    {
+        title: 'Envio del estado del juego',
+        challenge: 'Comunicar el estado del juego y de todos los jugadores a cada jugador.',
+        solution: 'Se implementó la estructura de datos GameStateMessage, contiene toda la informacion de estado actual del juego, tanto de jugadores como de eventos del juego, de esta manera toda la comunicacion del juego que envia el sevidor a los jugadores esta compactada en esta estructura.'
+    }
+];
+
+
+
+
+const accordionData = [
+    {
+        summary: 'GameMap',
+        details: 'Representa el mapa interactivo del juego y contiene las plataformas, balas activas, posiciones iniciales de los jugadores, límites del mapa e ItemSpawners. Contiene un metodo de actualizacion en el cual cada vez que se lo llama actualiza 1 frame de las los objetos que contiene, por ejemplo si hubiera balas activas, las llamaria a actualizar su posicion o si las plataformas no fueran estaticas tambien las actualizaria. Si se produjo alguna colision o estado de inconsistencia (salir de los limites del mapa o que el jugador haya actualizado su posicion para quedar dentro de una plataforma) llamara a la clase correspondiente para solucionarlo.',
+    },
+    {
+        summary: 'CollisionManager',
+        details: 'Detecta colisiones entre objetos haciendo uso de una pequeña clase Collider que la contienen todo lo que puede interactuar en el juego (jugadores, balas, plataformas y spawners). Usa modelo AABB. Tiene 2 maneras de determinar la colisiones, colision simple, informa con un "si o no" hubo colision y colision detallada, genera un struct que ademas de lo anterior te informa en donde sucedio la colision FOTO AQUI',
+    },
+    {
+        summary: 'CollisionEventManager',
+        details: 'Resuelve colisiones detectadas por CollisionManager. Corrige posiciones de jugadores si colisionan con plataformas y les informa si están sobre ellas. Gestiona interacciones de balas con jugadores, accionando según el tipo de bala (hay balas que infringen daño y otras que no), y maneja colisiones de balas con plataformas, tambien accionando segun el tipo de bala.',
+    },
+    {
+        summary: 'GameLoop',
+        details: 'Marca un orden el la realizacion de acciones del juego, esta clase no realiza ninguna accion, solo indica un orden, llamara a las clases necesarias para ejecutar las acciones. El orden es el siguiente: Obtiene un accion (Moverse o Disparar) de un jugador, Realiza los pasos para ejecutar la accion,llama a Atualizar el estado de todos los jugadores, aqui es donde se actualiza el juego 1 frame, luego de esto llama a Resolver las Posiciones de los jugadores por si quedaron en un estado invalido por lo anterior (fuera del mapa o colisionando con una plataforma), Luego Agrega las balas disparadas por los jugadores al GameMap para que este las pueda procesar como balas activas, llama a actualizar el estado del mapa, aqui es donde pasa 1 frame en el mapa y finalmente generar y enviar el mensaje de estado del juego, genera un GameStateMessage para que pueda ser enviado a los jugadores',
+    },
+    {
+        summary: 'GameStateMessage',
+        details: 'Contiene los datos actualizados del estado del juego, que son enviados a los jugadores. FOTO AQUI',
+    },
+];
+
+
 
 const GameLogic = () => {
     return (
-        <Box sx={{}}>
-            {/* Title */}
-            <Typography variant="h4" gutterBottom>
-                Game Logic
-            </Typography>
+        <Stack spacing={3}>
+            <Container>
+                <TitleAndDescription title="Overview"
+                    description={GameDescription} />
+            </Container>
 
-            {/* General Description */}
-            <Typography variant="body1" paragraph>
-                Este componente gestiona múltiples aspectos clave del juego, incluyendo la interacción
-                con los jugadores, las plataformas, las balas y los elementos del mapa. A continuación,
-                se detalla su funcionamiento y los principales componentes que lo conforman.
-            </Typography>
+            <Container>
+                <Typography variant="h5" gutterBottom>Challanges and Solutions</Typography>
+                <ChallengesAndSolutions data={challengesAndSolutions} />
+            </Container>
 
-            {/* Challenges and Solutions */}
-            <Typography variant="h5" gutterBottom>
-                Retos afrontados y soluciones realizadas
-            </Typography>
-            <Typography variant="body1" paragraph>
-                Garantizar un manejo eficiente de colisiones, la sincronización de las acciones de los
-                jugadores y la actualización en tiempo real de los elementos del mapa representaron los
-                principales desafíos. Estos fueron resueltos mediante el uso de estructuras optimizadas
-                y clases especializadas, como CollisionManager y CollisionEventManager.
-            </Typography>
 
-            {/* Additional Information */}
-            <Typography variant="h5" gutterBottom>
-                Información adicional
-            </Typography>
+            <Container>
+                <AccordionListWithHeader title="Informacion adicional de las clases" data={accordionData} />
+            </Container>
 
-            <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>GameMap</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Representa el mapa del juego y gestiona las plataformas, balas activas, posiciones
-                        iniciales de los jugadores, límites del mapa y los ItemSpawners. Es clave para
-                        mantener la integridad del entorno de juego y coordinar las interacciones.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-            <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>CollisionManager</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Identifica colisiones entre objetos con colliders usando el modelo AABB. Proporciona
-                        resultados detallados a través del struct CollisionResult, que incluye información
-                        sobre el área de colisión (superior, inferior, etc.).
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-            <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>CollisionEventManager</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Resuelve colisiones detectadas por CollisionManager. Maneja posiciones correctas de
-                        jugadores al colisionar, gestiona interacciones de balas con jugadores y plataformas,
-                        y aplica efectos específicos según el tipo de colisión.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-            <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>GameLoop</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Coordina las acciones de los jugadores e interactúa con otras clases como GameMap
-                        y PlayerController. Realiza tareas en un orden específico, como manejar las acciones
-                        de los jugadores, actualizar estados, resolver posiciones incorrectas, procesar
-                        disparos y generar mensajes del estado del juego.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-            <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>GameStateMessage</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Contiene los datos actualizados del juego y se utiliza para comunicar el estado a
-                        todos los jugadores. Incluye información procesada por el GameLoop y PlayerController.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-        </Box>
+        </Stack>
     );
 };
 
--- react/src/Pages/duckgame/Components/PlayerGameLogic.tsx
+++ react/src/Pages/duckgame/Components/PlayerGameLogic.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { Box, styled } from '@mui/material';
+import { Box, Stack, styled, Typography } from '@mui/material';
 import TitleAndDescription from "./TitleAndDescription";
 import ChallengesAndSolutions from "./ChallengesAndSolutions";
 import AccordionListWithHeader from "./AccordionList";
 
 
 const Container = styled(Box)(({ theme }) => ({
-    padding: theme.spacing(4, 4),
-    background: "grey",
-    [theme.breakpoints.down("sm")]: {
-        padding: theme.spacing(4, 2),
-    },
+    margin: 1,
+    marginBottom: 5,
+    background: "transparent",
 }));
 
 const gameInstanceAndPlayersDescription = `
-En esta segunda etapa del juego, la clase GameInstance es responsable de gestionar el juego y sus jugadores. Una vez que todos los jugadores se han conectado, el juego inicia y se sincroniza con ellos. GameInstance incluye componentes clave como el GameLoop, que controla el ciclo del juego, el PlayerController, que gestiona las acciones de los jugadores, y el PhysicsEngine, que maneja los movimientos y la física dentro del juego. Además, cada jugador está representado por la clase Player, que gestiona sus acciones, y se comunica a través de hilos dedicados para enviar y recibir datos. 
-La física del juego, incluida la simulación de movimientos y colisiones, se gestiona mediante el PhysicsEngine, que calcula las interacciones y movimientos de los jugadores en tiempo real.
-`;
+El sistema implementa un servidor de juego multijugador donde cada partida es manejada por una 
+GameInstance,
+que actúa como el núcleo central coordinando todos los aspectos del juego.
+Este componente gestiona la sincronización de los jugadores y controla el inicio de las partidas mediante 
+un sistema de múltiples hilos que manejan el bucle principal del juego y la comunicación con cada jugador.
+El sistema incorpora un motor de físicas personalizado que gobierna los movimientos de los jugadores y sus 
+interacciones con el entorno. Cada jugador opera con sus propios hilos de envío y recepción de datos, 
+mientras que un sistema de estados físicos y configuraciones preestablecidas garantiza la consistencia del 
+movimiento y las colisiones durante la partida.`;
 
 
 const challengesAndSolutions = [
@@ -26,13 +30,13 @@ const challengesAndSolutions = [
         solution: 'GameInstance asegura que todos los jugadores estén conectados antes de iniciar el juego, evitando que el juego comience hasta que todos estén listos.'
     },
     {
-        title: 'Manejo de Física y Movimientos',
-        challenge: 'Manejar la física y los movimientos de los jugadores en el entorno del juego.',
-        solution: 'El PhysicsEngine se encarga de calcular los movimientos de los jugadores, simular la fricción y gestionar las colisiones, garantizando una experiencia de juego fluida.'
+        title: 'Manejo de Física de Movimiento',
+        challenge: 'Manejar los movimientos de los jugadores en el entorno del juego para que sean fluidos y personalizables.',
+        solution: 'El PhysicsEngine se encarga de calcular los movimientos de los jugadores, simular la fricción y gestionar su interaccion con el suelo. Ademas implementa un struct para hacer que los movimientos sean mas personalizables.'
     },
     {
         title: 'Comunicación Eficiente entre Jugador y Servidor',
-        challenge: 'Comunicación eficiente entre el jugador y el servidor.',
+        challenge: 'Envio y recepcion de datos eficiente, poder enviar y recibir datos al mismo tiempo.',
         solution: 'La clase Player gestiona las acciones del jugador mediante hilos dedicados para enviar y recibir datos, asegurando que las interacciones se manejen de manera eficiente sin bloquear el proceso de juego.'
     }
 ];
@@ -40,35 +44,36 @@ const challengesAndSolutions = [
 const accordionData = [
     {
         summary: 'GameInstance',
-        details: 'Es responsable de contener el juego y los jugadores, y de sincronizar su entrada al juego. El juego no comienza hasta que todos los jugadores necesarios estén conectados, lo que puede generar problemas si algún jugador no se conecta. Incluye el bucle principal del juego (GameLoop), la clase que gestiona a los jugadores (PlayerController) y el motor de físicas (PhysicsEngine). Una vez que las condiciones se cumplen, lanza los hilos correspondientes para el juego y los jugadores.',
+        details: 'Incluye el bucle principal del juego (GameLoop), la clase que administra a los jugadores de la partida (PlayerController) y el motor de físicas (PhysicsEngine) que los jugadores utilizarán al comenzar la partida. Es el "contenedor" (tiene todos los datos y clases) de la partida del juego, una partida del juego se ejecuta dentro de un GameInstance lo que hace que cada partida (si se quiere) pueda tener sus propias reglas de fisicas (modificando la configuracion de PhysicsEngine.',
     },
     {
         summary: 'Player',
-        details: 'Clase encargada de gestionar las acciones de los jugadores, con métodos específicos para realizar dichas acciones. Utiliza un módulo de físicas (PhysicsEngine) y un módulo de colisiones, y a través de una interfaz permite el uso polimórfico de diversas armas.',
+        details: 'Clase encargada de gestionar las acciones de los jugadores (Disparar y moverse), con métodos específicos para realizar dichas acciones. Contiene toda la informacion del jugador en un momento especifico del juego (esta disparando o no, esta corriendo o no, velocidad en algun eje, etc.), para que las demas clases puedan usar dicha informacion para modificar su estado.',
     },
     {
         summary: 'PhysicsEngine',
-        details: 'Clase encargada de realizar los movimientos del jugador, configurada con los datos en un struct (PhysicsConfig) y proporcionando un struct (PhysicsState) que contiene los datos del jugador en cada momento. Utiliza estos datos y la posición del jugador para realizar movimientos y simular la fricción del jugador.',
+        details: 'Clase encargada de realizar los movimientos del jugador, pre-configurada con los datos de los valores de las fisicas en un struct (estructura de datos llamada PhysicsConfig) FOTO AQUI. Proporciona un struct (PhysicsState) personal para cada Player que contiene los datos necesarios para entender el estado del movimiento del jugador en cada momento FOTO AQUI. Gracias a estas dos estructuras de datos es que puede simular las fisicas del juego para los jugadores.',
     },
 ];
 
 
 const PlayerGameLogic = () => {
     return (
-        <>
+        <Stack spacing={3}>
             <Container>
-                <TitleAndDescription title="Start of the game and Players Logic"
+                <TitleAndDescription title="Overview"
                     description={gameInstanceAndPlayersDescription} />
             </Container>
 
             <Container>
+                <Typography variant="h5" gutterBottom>Challanges and Solutions</Typography>
                 <ChallengesAndSolutions data={challengesAndSolutions} />
             </Container>
 
             <Container>
                 <AccordionListWithHeader title="Informacion adicional de las clases" data={accordionData} />
             </Container>
-        </>
+        </Stack>
     );
 };
 
